ea8d52a8ffaa4ac3d60cf7dcaba85730119ce4a0
Samoht07 <341737@talnet.nl>
2020-02-11 11:31:40 +0100

    Uitwerking van de exercise

---
+++ script.js
@@ -0,0 +1,30 @@
+var vandaag = new Date();
+
+var date = vandaag.getHours() + "-" + (vandaag.getMinutes() + 1) + "-" + vandaag.getSeconds();
+document.getElementById("date").innerHTML = date;
+
+
+const pi = 3.14;
+
+function diaMeter() {
+    let diam = document.getElementById('number').value;
+
+    let answer = diam * pi;
+
+    document.getElementById("answer").innerText = answer;
+}
+
+function oppervlakte() {
+    let diam = document.getElementById('number').value;
+
+    let answer2 = diam * diam * pi * 0.25;
+
+    console.log(answer2);
+
+    document.getElementById("answer2").innerText = answer2;
+}
+
+function bereken() {
+    oppervlakte();
+    diaMeter();
+}
